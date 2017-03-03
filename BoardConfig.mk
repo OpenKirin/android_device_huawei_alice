7758dbf35da5c94eff9b831298f63bc1960302cb
Surdu Petru <surdu_petru@yahoo.com>
2017-03-03 14:06:52 +0100

    remove SuperSU from build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,5 +66,6 @@ RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
 BOARD_SUPPRESS_SECURE_ERASE := true
 RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_SUPERSU := true
 
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"

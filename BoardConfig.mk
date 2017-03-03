c543137fa4ba1a568b397676275c86d9137b0788
Surdu Petru <surdu_petru@yahoo.com>
2017-03-03 14:09:38 +0100

    add support for BRIGHTNESS & BATTERY

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,4 +71,5 @@ TW_EXCLUDE_SUPERSU := true
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd_backlight0/brightness
+TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery

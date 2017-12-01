4d928f1788cd89d71d2cccca7f44db31debfc5c4
Huawei-Dev <surdu_petru@yahoo.com>
2017-12-02 00:39:30 +0100

    include new product_launched_with_m.mk

--- full_alice.mk
+++ full_alice.mk
@@ -16,20 +16,14 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
+CM_BUILDTYPE := OpenKirin
 
 # Inherit from kiwi device
 $(call inherit-product, device/huawei/alice/device.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-
-PRODUCT_GMS_CLIENTID_BASE := android-huawei
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := full_alice
--- lineage.mk
+++ lineage.mk
@@ -14,8 +14,17 @@
 
 $(call inherit-product, device/huawei/alice/full_alice.mk)
 
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
+PRODUCT_GMS_CLIENTID_BASE := android-huawei
+
 PRODUCT_NAME := lineage_alice
-PRODUCT_DEVICE := alice
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="Huawei/ALE-L21/hwALE-H:6.0/HuaweiALE-L21/C432B596:user/release-keys" \

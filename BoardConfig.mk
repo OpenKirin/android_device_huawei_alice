233520ae0f8d35ec13be4f63c7fb9c2bdf480fe2
Huawei-Dev <surdu_petru@yahoo.com>
2018-03-02 18:47:43 +0100

    update Architecture

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ LOCAL_PATH := device/huawei/alice
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_2ND_ARCH := arm
